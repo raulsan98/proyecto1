b02920677f45ea24d55dab4e793bc8c092f73b9b
Usu2 Raul <raul.sanchez.mir@gmail.com>
2022-02-10 16:55:47 +0100

    Hago mi primer cambio

--- Prueba1/src/Prueba1/Prueba1.java
+++ Prueba1/src/Prueba1/Prueba1.java
@@ -3,7 +3,7 @@ package Prueba1;
 class Prueba1 {
 	 public static void main(String[] args) {
 		 
-		 System.out.println("Hola mundo java-git");
+		 System.out.println("Hola mundo java");
 		 
 	 }
 }
