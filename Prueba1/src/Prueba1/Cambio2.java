975aefae6d7d10a360408383e5de879ade0a5fe9
Usu2 Raul <raul.sanchez.mir@gmail.com>
2022-02-11 18:12:25 +0100

    Mensaje de validacion

---
+++ Prueba1/src/Prueba1/Cambio2.java
@@ -0,0 +1,11 @@
+package Prueba1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Clase cambio2 en proyecto2");
+		System.out.println("Clase cambio2 en proyecto2");
+		
+		
+	}
+}
